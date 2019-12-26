ce928cf2f83f7908bd9f5812d70be9be8d8e2dd1
rzh11111111 <294346639@qq.com>
2019-12-26 14:35:21 +0800

    更新

---
+++ src/components/hello2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1 style="color:red">
+          {{msg}}
+          </h1>
+  </div>
+</template>
+
+<script  lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  //private 它就不能在声明它的类的外部访问
+  // 
+  @Prop() private msg: string|undefined;
+}
+</script>
+
+<style>
+
+</style>
